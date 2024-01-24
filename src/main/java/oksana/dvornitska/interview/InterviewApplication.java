5609aa0387ede86a4e113dc2be3626feea1a6099
Oksana Dvornitska <userdvornicka@gmail.com>
2024-01-24 12:56:34 +0300

    add swagger

--- src/main/java/oksana/dvornitska/interview/InterviewApplication.java
+++ src/main/java/oksana/dvornitska/interview/InterviewApplication.java
@@ -1,9 +1,12 @@
 package oksana.dvornitska.interview;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Interview job parser API", version = "1.0"))
 public class InterviewApplication {
 
     public static void main(String[] args) {
